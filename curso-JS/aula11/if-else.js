c7b843b2c82e82bd7cf5c9c06f061271f0da7c61
antony-henrique-aluno <meugithub220@gmail.com>
2025-08-01 17:47:18 -0300

    update curso-JS/aula11/if-else.js

--- curso-JS/aula11/if-else.js
+++ curso-JS/aula11/if-else.js
@@ -1,5 +1,8 @@
 /*console.log("O document.write não funciona no console! No lugar dele, use o console.log")*/
 
 //Criando a primeira condição
-var velocidade = 65.5
-console.log(`A velocidade é de ${velocidade}Km/h`)
+var velocidade = 65
+console.log(`A velocidade é de ${velocidade}Km/h`);
+if (velocidade > 60) { //condição simples
+    console.log(`Você ultrapassou a velocidade permitida, você sera multado`);
+}
