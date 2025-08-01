ca043b9ca04cf69817580832c6759ba9280bfa1c
antony-henrique-aluno <meugithub220@gmail.com>
2025-08-01 17:58:58 -0300

    update curso-JS/aula11/ex009.js

--- curso-JS/aula11/ex009.js
+++ curso-JS/aula11/ex009.js
@@ -1,6 +1,6 @@
 var pais = "EUA"
 console.log(`Vivendo no ${pais}`)
-if (pais == "brasil"){ //Se o pais for diferent de brasil, ativa o else! Você estrangeiro
+if (pais == "brasil"){ //Se o pais for diferente do brasil, ativa o else! Você estrangeiro
     console.log('Você é brasileiro');
 }else 
     console.log(`Você é estrangeiro`);
