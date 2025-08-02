653f35b3c298676efc757d122c2163a702122c46
antony-henrique-aluno <meugithub220@gmail.com>
2025-08-02 17:52:39 -0300

    update curso-JS/aula12/ex013.js

--- curso-JS/aula12/ex013.js
+++ curso-JS/aula12/ex013.js
@@ -1,8 +1,8 @@
 //Condições multipla.
 
 //Condições multipla serve para valores fixos. 
-É muito util no mundo da programação, principalmente
-quando eu quero trabalhar com valores fixos, como eu
-acabei de falar. Ela não serve muito para intervalo 
-de valores como a gente exemplificou ali: o bom dia, 
-boa tarde e boa noite.
+// É muito util no mundo da programação, principalmente
+// quando eu quero trabalhar com valores fixos, como eu
+// acabei de falar. Ela não serve muito para intervalo 
+// de valores como a gente exemplificou ali: o bom dia, 
+// boa tarde e boa noite. Essa  
