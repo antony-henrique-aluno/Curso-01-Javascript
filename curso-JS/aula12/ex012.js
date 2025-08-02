be0be5c2ebc55d00351215b0c5462c8ac3ba0b74
antony-henrique-aluno <meugithub220@gmail.com>
2025-08-02 17:40:29 -0300

    update curso-JS/aula12/ex012.js

--- curso-JS/aula12/ex012.js
+++ curso-JS/aula12/ex012.js
@@ -1,8 +1,9 @@
-var hora = 8 
+var agora = new Date()
+var hora = agora.getHours()
 if (hora < 8) {
     console.log("Bom dia!")
 }else if(hora <= 18){
     console.log("Boa tarde!")
 }else{
     console.log("Boa noite!")
-}
+} 
