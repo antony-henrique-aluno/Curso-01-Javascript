25ada6038ae3c0376b83a37710f93a8408d9ea2d
antony-henrique-aluno <meugithub220@gmail.com>
2025-08-02 18:13:02 -0300

    update curso-JS/aula12/ex013.js

--- curso-JS/aula12/ex013.js
+++ curso-JS/aula12/ex013.js
@@ -11,11 +11,36 @@
 var agora = new Date()
 var diaSemana = agora.getDay()
 
-/*0 Domingo
-1 Segunda 
-2 Terça
-3 Quarta
-4 Quinta
-5 Sexta
-6 Sabado */
+/*  0 Domingo
+    1 Segunda 
+    2 Terça
+    3 Quarta
+    4 Quinta
+    5 Sexta
+    6 Sabado */
 
+switch (diaSemana) {
+    case 0:
+        console.log("Domingo")
+        break; //A função break é obrigatoria na hora de usar o switch
+    case 1:
+        console.log("Segunda-Feira")
+        break;
+    case 2:
+        console.log("Terça-Feira") 
+        break;
+    case 3:
+        console.log("Quarta-Feira") 
+        break;
+    case 4:
+        console.log("Quinta-Feira")
+        break;
+    case 5:
+        console.log("Sexta-feira")
+        break;
+    case 6:
+        console.log("Sabado")
+        break;
+    default:
+        
+}
